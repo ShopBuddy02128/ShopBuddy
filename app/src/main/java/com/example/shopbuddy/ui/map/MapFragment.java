bacf73ac5584202eee0ea0c6c7d433df03adccb0
PrimtalPrimdahl <s204471@student.dtu.dk>
2021-06-14 22:04:48 +0200

    Kort med brugerplacering

--- app/src/main/java/com/example/shopbuddy/ui/map/MapFragment.java
+++ app/src/main/java/com/example/shopbuddy/ui/map/MapFragment.java
@@ -4,16 +4,20 @@ import android.Manifest;
 
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
-import android.location.LocationManager;
+import android.location.Location;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import androidx.lifecycle.ViewModelProvider;
@@ -22,21 +26,37 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.shopbuddy.R;
 import com.example.shopbuddy.databinding.FragmentMapBinding;
 
+
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 
 import org.jetbrains.annotations.NotNull;
+
+
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
     private MapViewModel mapViewModel;
     private FragmentMapBinding binding;
     private GoogleMap map;
     private MapView mapView;
+    private final static int LOCATION_PERMISSION = 1;
+    Task<Location> locationTask;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
-
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getContext());
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -49,6 +69,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         return root;
     }
 
+
+
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -60,9 +82,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             mapView.getMapAsync(this);
 
         }
-
-
     }
+
+
 
     @Override
     public void onDestroyView() {
@@ -70,16 +92,40 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         binding = null;
     }
 
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
-        MapsInitializer.initialize(getContext());
-        map = googleMap;
-        //map.setMyLocationEnabled(true);
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    private void checkPermission(){
+        if(ContextCompat.checkSelfPermission(this.getContext(),Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            map.setMyLocationEnabled(true);
+            locationTask = fusedLocationProviderClient.getLastLocation();
+            zoomToUserLocation();
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this.getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)){
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION);
+            } else {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION);
+            }
+        }
 
     }
 
 
+    private void zoomToUserLocation(){
+        locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+            }
+        });
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        map = googleMap;
+        checkPermission();
+
+
+    }
 
 }
