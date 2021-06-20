cf00212af341e0b0c8479bfc096334166565f075
AmalieSommer <77000762+AmalieSommer@users.noreply.github.com>
2021-06-20 16:45:40 +0200

    Code cleaning

--- app/src/main/java/com/example/shopbuddy/ui/notifications/AlarmAdapter.java
+++ app/src/main/java/com/example/shopbuddy/ui/notifications/AlarmAdapter.java
@@ -5,17 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
 import android.widget.CheckedTextView;
-import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import com.example.shopbuddy.R;
-import com.example.shopbuddy.models.AlarmItem;
-import com.google.android.material.transition.Hold;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class AlarmAdapter extends ArrayAdapter<String> {
 
